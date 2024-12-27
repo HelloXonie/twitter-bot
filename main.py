0c38f7377b3f3bd74253a317f65a7c0a569fbb8c
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2024-12-26 23:27:00 -0800

    few added lines

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 #!/bin/python3
 
 import tweepy
+from dotenv import load_dotenv
 print(tweepy.__version__)
+
+
+client = tweepy.Client(
+        consumer_key=,
+        consumer_secret=,
+        access_token=,
+        access_token_secret=,
+        )
+
+
