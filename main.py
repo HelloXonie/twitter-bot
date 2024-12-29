8a0373eca7d8f43c9a58b656dc7d35f448d936f5
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2024-12-29 00:01:51 -0800

    testing keys from .env

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-CONSUMER_KEY=os.getenv()
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
 
 print(CONSUMER_KEY)
 print(tweepy.__version__)
 
 
-client = tweepy.Client(
-        consumer_key=,
-        consumer_secret=,
-        access_token=,
-        access_token_secret=,
-        )
+#client = tweepy.Client(
+#        consumer_key=,
+#        consumer_secret=,
+#        access_token=,
+#        access_token_secret=,
+#        )
 
 
